8285979225a9aadef471a93b29ab46b77098e1c4
raj8888 <jadhavrj8877@gmail.com>
2022-09-03 20:50:02 +0530

    Mathematical Operator

---
+++ Mathematical-Operator/problem1.js
@@ -0,0 +1,7 @@
+let one=1*2;
+let two=2;
+let three=3;
+let four=4;
+let five=5;
+let product=one*two*three*four*five;
+console.log(product)
---
+++ Mathematical-Operator/problem3.js
@@ -0,0 +1,8 @@
+let one=1;
+let two=2*2;
+let three=3*3;
+let four=4*3;
+let five=5*2;
+let six=6*2;
+let product=one+two+three+four+five+six;
+console.log(product)
