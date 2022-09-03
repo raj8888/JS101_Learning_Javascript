3a28b2cbb1e69efc12a8bed7b5aef1e9de8c778c
raj8888 <jadhavrj8877@gmail.com>
2022-09-03 20:40:52 +0530

    Variables Problem

---
+++ Chap1Variables/problem4.js
@@ -0,0 +1,37 @@
+//Store the name, school, grade, section, roll and the marks scored by the student in 3 subjects
+//Print the report card of the student (You can make it look nice by using some keyboard symbols )
+//Explore ASCII ART (https://en.wikipedia.org/wiki/ASCII_art (Links to an external site.)) or Text Art (https://fsymbols.com/text-art/ (Links to an external site.)) for some inspiration
+
+var RC = "     ***** Report Card ******";
+var name = "* Student name: Rajvardhan Jadhav.";
+var school = "* School Name : Masai School.";
+var roll ="* Roll No     : fw22_0388.";
+var grade="* Grade       : B.";
+var section="* Section     : A.";
+var blank = " ";
+var title ="Sr.No.   Subject Name   Out of 100"
+var sub1 = "1.        JS-101          98";
+var sub2 = "2.        DSA-101         95";
+var sub3 = "3.        CC-101          96";
+var end = "       ***** Thank You *****"
+console.log(blank);
+console.log(blank);
+console.log(RC);
+console.log(blank);
+console.log(name);
+console.log(school);
+console.log(roll);
+console.log(grade);
+console.log(section);
+console.log(blank);
+console.log(title);
+console.log(blank);
+console.log(sub1);
+console.log(blank);
+console.log(sub2);
+console.log(blank);
+console.log(sub3);
+console.log(blank);
+console.log(end);
+console.log(blank);
+
