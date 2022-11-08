a3ec3afb3af5aa4451406528359f24b136399fe3
Rajvardhan <82108762+raj8888@users.noreply.github.com>
2022-11-08 13:09:01 +0530

    Create tryraj1.js

---
+++ tryraj1.js
@@ -0,0 +1,13 @@
+//Define a variable called name with your Name as the assigned value
+//Print the value stored in the variable name
+//Change the variable to store your father's name
+//Print the value stored in the variable name
+//Change the variable again to store your mother's name.
+//Print the value stored in the variable name
+
+var name="Rajvardhan";
+console.log(name);
+var fathername="Yashwant";
+console.log(fathername);
+var mothername="Archana";
+console.log(mothername);
